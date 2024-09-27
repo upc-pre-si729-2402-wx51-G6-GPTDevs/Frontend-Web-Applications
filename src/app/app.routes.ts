7fc19d6af50188d085097364f3392719a2dcab0a
Daniel-10806 <U202211108@upc.edu.pe>
2024-09-27 16:46:45 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WorkDoneComponent } from './work-done/work-done.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WorkDoneComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
