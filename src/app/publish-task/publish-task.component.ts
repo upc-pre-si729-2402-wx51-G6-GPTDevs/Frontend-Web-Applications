91db854e2314a5a82071cddc5c9beeb8833f5b41
Franco Durand <francoduve.25@gmail.com>
2024-09-27 19:41:45 -0500

    feat: add route

--- src/app/publish-task/publish-task.component.ts
+++ src/app/publish-task/publish-task.component.ts
@@ -1,10 +1,11 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-publish-task',
   templateUrl: './publish-task.component.html',
-  imports: [NgIf],
+  imports: [NgIf, RouterLink],
   standalone: true,
   styleUrls: ['./publish-task.component.scss']
 })
