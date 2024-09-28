e84e811061d6b6b2bd84c7be6c9fae846dcebf74
Daniel-10806 <U202211108@upc.edu.pe>
2024-09-27 19:26:20 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PublishTaskComponent } from './publish-task/publish-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PublishTaskComponent }  // Ruta principal
+];
