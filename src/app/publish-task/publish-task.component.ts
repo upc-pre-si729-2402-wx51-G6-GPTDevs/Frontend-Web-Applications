2d8b8c4836d1f63a584bdcdb4a5e8f02cdfde670
Daniel-10806 <U202211108@upc.edu.pe>
2024-09-27 19:21:56 -0500

    Create publish-task.component.ts

---
+++ src/app/publish-task/publish-task.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publish-task',
+  templateUrl: './publish-task.component.html',
+  standalone: true,
+  styleUrls: ['./publish-task.component.scss']
+})
+export class PublishTaskComponent {
+  // Variable para almacenar el mensaje de éxito
+  successMessage: string = '';
+
+  // Lógica del componente
+  onSubmit() {
+    this.successMessage = 'Formulario Publicado'; // Establece el mensaje de éxito
+    console.log('Formulario enviado');
+  }
+
+  onFileUpload(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files) {
+      console.log('Subiendo archivo...', target.files);
+      // Aquí puedes añadir la lógica para manejar los archivos subidos
+    }
+  }
+}
