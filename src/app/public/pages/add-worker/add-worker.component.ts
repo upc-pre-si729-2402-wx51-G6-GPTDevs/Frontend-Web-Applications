aa237d25f6d7220eb5b4b46721af1dd18e5a0f46
TonyRamosNajar <u20211a153@upc.edu.pe>
2024-10-23 00:45:01 -0500

    feat: update profile users

---
+++ src/app/public/pages/add-worker/add-worker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-worker',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-worker.component.html',
+  styleUrl: './add-worker.component.css'
+})
+export class AddWorkerComponent {
+
+}
